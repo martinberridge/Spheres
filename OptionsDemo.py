54197c2f416016c2e6ad98bf0684f735c73ad6c4
martin berridge <martin.berridge@gmail.com>
2015-11-09 17:00:13 +0000

    override market nodes and tweak

--- OptionsDemo.py
+++ OptionsDemo.py
@@ -8,16 +8,18 @@ import numpy as np
 from dateutil.parser import parse
 from datetime import  datetime
 import math
+
 import QuantLib as ql
+print "couldn't load QuantLib - check installation"
+
 import dag
+import graphfunctions
 import visualize
 
 # HIST_FILE = 'C:\Users\mhna\Documents\Python\RiskLab\sa\HistPrices.csv'
 # VOL_FILE =  'C:\Users\mhna\Documents\Python\RiskLab\sa\VolParameters.csv'
 HIST_FILE = '~/Downloads/HistPrices.csv'
 VOL_FILE = '~/Downloads/VolParameters.csv'
-
-visualize.enable()
 
 class EquityMarketData(object):
 
@@ -86,9 +88,9 @@ class OptionsMarket(dag.DomainObj):
         return self.ticker + "_" + str(self.id)
 
     @dag.DagMethod
-    def vol(self, strike = 1, tenor = 1, strike_type = 'pct'):
-        return self.market_data.get_vol(self.ticker,spot,strike,tenor,strike_type) + self.tweak()
-
+    def vol(self, spot, strike = 1, tenor = 1, strike_type = 'pct'):
+        tweaked_vol = self.market_data.get_vol(self.ticker,spot,strike,tenor,strike_type) + self.tweak()
+        return tweaked_vol
 
 class EquityMarket(dag.DomainObj):
 
@@ -152,71 +154,81 @@ class EuropeanOption(dag.DomainObj):
         return ql.Date(datetime.now().day,datetime.now().month,datetime.now().year)
 
     @dag.DagMethod
-    def PV(self, SpotOverRide = None, VolOverRide = None, VolOverRideType = 'shock'):
-
-        if SpotOverRide != None:
-            current_spot = SpotOverRide
-        else:
-            current_spot = self.equity_market().spot()
-
+    def PV(self):
+        current_spot = self.equity_market().spot()
         spot = ql.SimpleQuote(current_spot)
 
-        if VolOverRide != None:
-            if VolOverRideType.upper() == 'SHOCK':
-                strike_vol = self.options_market().vol(self.strike,tenor = (self.expiry - self.value_date() ), strike_type = 'abs')+VolOverRide
-            else:
-                strike_vol = VolOverRide
-
-        else:
-            strike_vol = self.options_market().vol(self.strike,tenor = (self.expiry - self.value_date() ), strike_type = 'abs')
-
+        strike_vol = self.options_market().vol(self.strike,current_spot,tenor = (self.expiry - self.value_date() ), strike_type = 'abs')
         sigma = ql.SimpleQuote(strike_vol)
+
         r = ql.SimpleQuote(self.rates_market().spot())
 
-        option = ql.EuropeanOption(ql.PlainVanillaPayoff(self.opt_type,self.strike),ql.EuropeanExercise(self.expiry))
 
         riskFreeCurve = ql.FlatForward(0,ql.TARGET(),ql.QuoteHandle(r),ql.Actual360())
         volatility = ql.BlackConstantVol(0,ql.TARGET(), ql.QuoteHandle(sigma),ql.Actual360())
         process = ql.BlackScholesProcess(ql.QuoteHandle(spot),
                                          ql.YieldTermStructureHandle(riskFreeCurve),
                                          ql.BlackVolTermStructureHandle(volatility))
+
         engine = ql.AnalyticEuropeanEngine(process)
 
+        option = ql.EuropeanOption(ql.PlainVanillaPayoff(self.opt_type,self.strike),ql.EuropeanExercise(self.expiry))
         option.setPricingEngine(engine)
-        return option.NPV()
+        PV = option.NPV()
+        return PV
 
 
-emd = EquityMarketData()
-omd = OptionMarketData()
-em = EquityMarket("XOM",emd)
-om = OptionsMarket("XOM",omd)
-rm = RatesMarket()
 
-spot = em.spot()
-print spot
-print om.vol(spot)
+def main():
+    emd = EquityMarketData()
+    omd = OptionMarketData()
+    em = EquityMarket("XOM",emd)
+    om = OptionsMarket("XOM",omd)
+    rm = RatesMarket()
 
-option1 = EuropeanOption("XOM",'put',72,'25/12/2015')
-option1.equity_market.set_value(em)
-option1.options_market.set_value(om)
-option1.rates_market.set_value(rm)
-option1.value_date.set_value(ql.Date())
+    option1 = EuropeanOption("XOM",'put',72,'25/12/2015')
+    option1.equity_market.set_value(em)
+    option1.options_market.set_value(om)
+    option1.rates_market.set_value(rm)
+    option1.value_date.set_value(ql.Date(9,11,2015))
 
-print option1.PV()
+    print option1.PV()
 
-#print option1.PV(SpotOverRide=70, VolOverRide = 0.40)
 
-spotRange = np.linspace(-.10,.10,5)
-volRange = np.linspace(-0.05,0.05,3)
+    spotRange = np.linspace(-.10,.10,5)
+    volRange = np.linspace(-0.05,0.05,3)
 
-optionPV = []
-for j in volRange:
-    spotLadder = []
+    optionPV = []
     spot  = em.spot()
-    for i in spotRange:
-        spotLadder.append(option1.PV(SpotOverRide = spot*(1+i), VolOverRide = j))
-    optionPV.append(spotLadder)
+
+    # tweak by overriding spot/vol tweak values
+    for j in volRange:
+        spotLadder = []
+        for i in spotRange:
+            em.tweak.set_value(spot * i)
+            om.tweak.set_value(j)
+            spotLadder.append( option1.PV() )
+        optionPV.append(spotLadder)
+
+    print optionPV
+
+    #clean up
+    optionPV = []
+
+    #tweak the markets using context/tweaks - reprice options dependent on these markets
+    for j in volRange:
+        spotLadder = []
+
+        for i in spotRange:
+            with graphfunctions.context():
+                graphfunctions.tweak( em.tweak,(spot * i) )
+                graphfunctions.tweak( om.tweak, j )
+                spotLadder.append( option1.PV())
+
+        optionPV.append(spotLadder)
+
+    print optionPV
 
 
-print optionPV
-
+if __name__ == "__main__" :
+    main()
